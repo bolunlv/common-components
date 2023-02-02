33527a8f5068c09fc1c86d0be47845e937ec8dfa
lvbolun <bolun.lu@eoitek.com>
2023-02-02 15:40:38 +0800

    测试文档

---
+++ docs/components/eoi-tab/EOITabExample.vue
@@ -0,0 +1,43 @@
+<template>
+  <EOITab :active-tab="activeTab" :tabs-data="tabsData" @handle-click="handleClick">
+    <template v-for="(screen, index) in screenData" :key="index" #[screen.name]>
+        <component :is="screen.type" />
+    </template>
+  </EOITab>
+</template>
+<script setup lang="ts">
+  import EOITab from '../../../componentss/component/EoiPlus';
+  import { ref, markRaw } from 'vue'
+  import { ElInput, ElDatePicker } from 'element-plus';
+
+  const screenData = ref([
+   {
+      name: 'Tab1',
+      type: markRaw(ElInput),
+   },
+   {
+      name: 'Tab2',
+      type: markRaw(ElDatePicker),
+   },
+  ])
+
+  const tabsData = ref([
+    {
+      label: 'Tab1', 
+      name: 'Tab1', 
+      disabled: false,
+    },
+    {
+      label: 'Tab2', 
+      name: 'Tab2', 
+      disabled: false,
+    },
+  ])
+
+  const activeTab = ref('Tab1')
+  const handleClick = (val: any) => {
+    console.log(val, '标签页')
+  }
+</script>
+
+
